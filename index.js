769a1a89657b5818ff4da3a907fc028631c3a8fd
masesm5@yahoo.com <maseskrikorian@gmail.com>
2021-09-07 22:38:11 -0700

    fix parsing

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
         const searchArea = document.querySelector("[data-cid]");
         const imgs = searchArea.getElementsByTagName("img")
         var i;
+        var index = 0;
         var total = 0;
         for (i = 0; i < imgs.length; i++) {
             total += 1
@@ -34,14 +35,25 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
                 continue
             }
             imgs[i].click()
-            text.innerHTML = `Parsing Images...[ ${total} ]`
+            text.innerHTML = `Parsing Images...[ ${total} / ${num} ]`
             await sleep(message.speed * 1000).then(() => {
-                var el = document.querySelector("[style='opacity: 1;']")
-                urls.push(el.getElementsByTagName("img")[1].src)
+                const area = document.querySelectorAll("[role='link'][target='_blank'");
+                if(area === undefined || area.length < 3){
+                    return
+                }
+                if(area[index].firstChild === undefined){
+                    return
+                }
+                const imgLink = area[index].firstChild
+                if(imgLink === undefined || imgLink.src === undefined){
+                    return
+                }
+                urls.push(imgLink.src)
             })
             if(i === num-1){
                 break;
             }
+            index = 2
         }
         enableScrolling()
         overlay.remove()
@@ -68,7 +80,7 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
         overlay.setAttribute("id", "pgd_overlay")
         text.setAttribute("style", "color: white; padding-top: 25%; font-size: 55px; justify-content: center; align-items: center;text-align: center; vertical-align: middle;")
         btn.setAttribute("style", "padding: 20px; cursor: pointer; margin-top: 30px; margin-left: calc(50% - 150px); border-radius: 25px; color: dimgrey; font-size: 30px; justify-content: center; align-items: center; text-align: center; vertical-align: middle; width: 300px; background-color: aliceblue;")
-        text.innerHTML = "Parsing Images...[ 0% ]"
+        text.innerHTML = "Parsing Images...[ 0 ]"
         btn.innerHTML = "STOP PARSING"
         overlay.setAttribute("style", "overflow: hidden; position: fixed; width: 100%; height: 100%; top: 0; left: 0; right: 0; bottom: 0; background-color: rgba(0,0,0,0.7); z-index: 999; cursor: pointer;")
         disableScrolling()
